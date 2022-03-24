481501b4d40312dbe4b4f47d5712606782e3de0e
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2022-03-24 19:15:00 +0100

    Implemented change of class of active tab in the left panel

--- client/src/components/LeftPanel/LeftPanel.jsx
+++ client/src/components/LeftPanel/LeftPanel.jsx
@@ -26,12 +26,24 @@ const LeftPanel = ({ handleTodosToShow }) => {
     }
   };
 
+  const toggleClass = (tab, e) => {
+    tab.className = "item";
+    if (tab.value === e.target.value) {
+      tab.className = "item active";
+    }
+  };
+
+  const handleMenuClick = (e) => {
+    handleTodosToShow(e);
+    tabsRefs.current.forEach((tab) => toggleClass(tab, e));
+  };
+
   return (
     <div className="left-panel">
       <div className="logo">.todo</div>
       <div className="items">
         {tabs.map((tab) => (
-          <button key={tab.value} ref={addToRefs} className={tab.className} value={tab.value} onClick={handleTodosToShow}>
+          <button key={tab.value} ref={addToRefs} className={tab.className} value={tab.value} onClick={handleMenuClick}>
             {tab.value}
           </button>
         ))}
