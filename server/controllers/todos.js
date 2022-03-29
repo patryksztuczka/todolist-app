b5b7f9654c74066dfa9f90ebdc58b41de89cec77
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2022-03-29 16:54:09 +0200

    Implemented getAll, create, getOne and delete controllers

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,24 +1,52 @@
 const Todo = require('../models/Todo');
 
-const getAllTodos = (req, res) => {
-  res.send("get all tasks");
+const getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.status(200).json({ todos: todos }); //{ todos } === { todos: todos } - ES6 Syntax
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTodo = async (req, res) => {
-  const todo = await Todo.create(req.body);
-  res.status(201).json({ todo });
+  try {
+    const todo = await Todo.create(req.body);
+    res.status(201).json({ todo });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
 };
 
-const getTodo = (req, res) => {
-  res.json({ id: req.params.id });
+const getTodo = async (req, res) => {
+  try {
+    const { id: todoId } = req.params;
+    const todo = await Todo.findOne({ _id: todoId });
+    if (!todo) {
+      return res.status(404).json({ msg: `No task with id: ${todoId}` });
+    }
+    res.status(200).json({ todo });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTodo = (req, res) => {
   res.send("update todo");
 };
 
-const deleteTodo = (req, res) => {
-  res.send("delete todo");
+const deleteTodo = async (req, res) => {
+  try {
+    const { id: todoId } = req.params;
+    const todo = await Todo.findOneAndDelete({ _id: todoId });
+    if (!todo) {
+      return res.status(404).json({ msg: `No task with id: ${todoId}` });
+    }
+    res.status(200).json({ todo });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
