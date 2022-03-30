3f8ae167d38402097dde144db50f19693a3fd3a9
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2022-03-30 19:42:51 +0200

    Added function for fetching from api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
-  return <div className="app">Hello</div>;
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/todos");
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div className="app">Helloo</div>;
 };
 
 export default App;
